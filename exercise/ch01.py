4f0a2f8308822843a59eccbc4332303183c53893
oishigyunyu <yamamuuuura@gmail.com>
2021-09-22 23:00:48 +0900

    modified

--- exercise/ch01.py
+++ exercise/ch01.py
@@ -45,4 +45,4 @@ class FieldElement:
 
 if __name__ == '__main__':
     for prime in [7, 11, 17, 31]:
-        print([pow(i, prime-1, prime) for i in range(1, prime)])
+        print([pow(i, prime - 1, prime) for i in range(1, prime)])
