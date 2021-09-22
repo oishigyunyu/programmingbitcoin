14b6c9d858497d1b82d44127cdda3bac37249195
oishigyunyu <yamamuuuura@gmail.com>
2021-09-22 23:26:14 +0900

    ex1

---
+++ exercise/ch02.py
@@ -0,0 +1,21 @@
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = y
+        self.y = y
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve.'.format(x, y))
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
+
+if __name__ == '__main__':
+    def on_curve(x, y):
+        return y**2 == x**3 + 5 * x + 7
+
+    print(on_curve(2,4))
+    print(on_curve(-1,-1))
+    print(on_curve(18,77))
+    print(on_curve(5,7))
